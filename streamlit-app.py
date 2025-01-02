00014794ebb1bfaec2236311beb9c3a1f29f1ead
gauravpatil-1127 <gaurav.prashant.patil@gmail.com>
2025-01-02 22:35:59 +0530

    modify

--- streamlit-app.py
+++ streamlit-app.py
@@ -3,65 +3,52 @@ import google.generativeai as genai
 import os
 import PyPDF2 as pdf
 from dotenv import load_dotenv
-load_dotenv()  #load all the evironment variable
+import json
+
+load_dotenv() ## load all our environment variables
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-#Gemini Pro Response
-
-def get_gemini_response(input):
-    model = genai.GenerativeModel(model_name='gemini-pro')
-    response = model.generate_content(input)
+def get_gemini_repsonse(input):
+    model=genai.GenerativeModel('gemini-pro')
+    response=model.generate_content(input)
     return response.text
 
-
-def input_pdf_txt(uploaded_file):
-    # Initialize an empty string for the extracted text
-    text = ""
-    
-    # Create a PdfReader object
-    reader = pdf.PdfReader(uploaded_file)
-    
-    # Iterate over all pages in the PDF
-    for page in reader.pages:
-        # Extract and append the text from each page
-        text += page.extract_text() or ""  # Handle cases where extract_text returns None
-    
+def input_pdf_text(uploaded_file):
+    reader=pdf.PdfReader(uploaded_file)
+    text=""
+    for page in range(len(reader.pages)):
+        page=reader.pages[page]
+        text+=str(page.extract_text())
     return text
 
+#Prompt Template
 
+input_prompt="""
+Hey Act Like a skilled or very experience ATS(Application Tracking System)
+with a deep understanding of tech field,software engineering,data science ,data analyst
+and big data engineer. Your task is to evaluate the resume based on the given job description.
+You must consider the job market is very competitive and you should provide 
+best assistance for improving thr resumes. Assign the percentage Matching based 
+on Jd and
+the missing keywords with high accuracy
+resume:{text}
+description:{jd}
+
+I want the response in one single string having the structure
+{{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
+"""
 
 ## streamlit app
-st.title("Application Tracking System")
-st.text("Improve your resume using this tool")
-job_description = st.text_area("Paste the Job Description Here")
-upload_file = st.file_uploader("upload your resume",type="pdf",help="Please upload the pdf only")
-
+st.title("Smart ATS")
+st.text("Improve Your Resume ATS")
+jd=st.text_area("Paste the Job Description")
+uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
 
 submit = st.button("Submit")
 
 if submit:
-    if upload_file is not None:
-        text = input_pdf_txt(uploaded_file=upload_file)
-        #Prompt Template
-        input_prompt =f'''
-        I have a job description and a candidate's resume. I need the model to provide the following outputs:
-
-        Percentage Match: Calculate the percentage match between the job description and the candidate's resume.
-        Profile Summary: Summarize how well the candidate's resume aligns with the job description, highlighting key strengths or areas of alignment.
-        Missing Keywords: Identify keywords or key skills from the job description that are not present in the candidate's resume and suggest them for inclusion.
-        Job Description: {job_description}
-
-        Candidate Resume: {text}
-
-        Please provide the results as follows:
-
-        Percentage Match: [Calculated match percentage]
-        Profile Summary: [Summary of match]
-        Missing Keywords: [List of missing keywords]
-
-        Do not hallicunate with anything else also don't generate any random response you need to compare job description and candidate resume text if you are not getting any text please say "provide text"
-
-        '''
-        response = get_gemini_response(input_prompt)
+    if uploaded_file is not None:
+        text=input_pdf_text(uploaded_file)
+        response=get_gemini_repsonse(input_prompt)
         st.subheader(response)
